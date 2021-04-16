1eb35893fea3225068babd9a0320d39084bfb1bc
Michael <stitlesscms@gmail.com>
2021-04-15 20:24:19 -0400

    GubarevIndividual

---
+++ Assets/PlatformRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlatformRotation : MonoBehaviour
+{
+    
+    public GameObject platform;
+    void Update() 
+    {
+        if(Input.touchCount == 1)
+        {            
+            Touch touchZero = Input.GetTouch(0);  
+            if(touchZero.phase == TouchPhase.Began)
+            {
+                platform.transform.rotation = Quaternion.identity;
+            }                
+        }    
+    }
+}
